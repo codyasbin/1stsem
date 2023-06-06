3abbaa82b4df457b096b5323b72569d99cb5ec9f
codyasbin <himalthapa346@gmail.com>
2023-06-06 08:30:01 +0545

    staticmethod

---
+++ guii.py
@@ -0,0 +1,21 @@
+#create an login system in gui using python
+#importing tkinter module
+from tkinter import *
+#importing messagebox module
+from tkinter import messagebox
+#importing sqlite3 module
+import sqlite3
+#importing random module
+import random
+#importing time module
+import time
+#creating a class
+class Login:
+    #creating a constructor
+	<issue_comment>username_1: @username_0
+def __init__(self, username_0):
+	<issue_comment>username_0: @username_1
+<issue_comment>username_0: @username_1
+<issue_comment>username_0: @username_1
+<issue_comment>username_0: @username_1
+<issue_comment>username_0: @username_1
